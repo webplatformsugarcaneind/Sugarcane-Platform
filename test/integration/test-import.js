d3c8aa212a1b154a7db38f01c8fc8171282e7be7
dineshdhayfule <dineshdhayfule@gmail.com>
2026-05-11 11:30:09 +0530

    chore: ignore md and test folders

--- test/integration/test-import.js
+++
@@ -1,7 +0,0 @@
-// Test if updateWorkerAvailability is properly exported
-const {
-    updateWorkerAvailability
-} = require('./controllers/hhm.controller');
-
-console.log('updateWorkerAvailability:', updateWorkerAvailability);
-console.log('Type:', typeof updateWorkerAvailability);
